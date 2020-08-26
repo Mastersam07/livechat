1ebd3fe8461c4d16c7e8c3ccba6b35a2d1036c30
Mastersam <abadasamuelosp@gmail.com>
2020-08-26 12:19:41 -0700

    added ios folder

---
+++ ios/Classes/LivechatPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface LivechatPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/LivechatPlugin.m
@@ -0,0 +1,15 @@
+#import "LivechatPlugin.h"
+#if __has_include(<livechat/livechat-Swift.h>)
+#import <livechat/livechat-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "livechat-Swift.h"
+#endif
+
+@implementation LivechatPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftLivechatPlugin registerWithRegistrar:registrar];
+}
+@end
