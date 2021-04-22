773526514b191e3a6761bc3467177212c8458046
Samuel Abada <abadasamuelosp@gmail.com>
2021-04-22 11:21:43 -0700

    :bug: Fix: Ios issues
    Fixed issues relating to podspec and pod install

--- ios/Classes/LivechatPlugin.m
+++ ios/Classes/LivechatPlugin.m
@@ -1,11 +1,11 @@
 #import "LivechatPlugin.h"
 #if __has_include(<livechat/livechat-Swift.h>)
-#import <livechat/livechat-Swift.h>
+#import <livechatt/livechatt-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "livechat-Swift.h"
+#import "livechatt-Swift.h"
 #endif
 
 @implementation LivechatPlugin
